2d27b54d57998505dbbbf3509fcd2056f8093d41
Davide Garberi <dade.garberi@gmail.com>
2020-02-14 22:20:19 +0100

    lavender: Decommonize FM
    Change-Id: I6ba214893ccaa7be8b506fe6347e2dff1e8dcc90

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,11 @@ DEVICE_PATH := device/xiaomi/lavender
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
+# FM
+AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
+BOARD_HAS_QCA_FM_SOC := cherokee
+BOARD_HAVE_QCOM_FM := true
+
 # Kernel
 TARGET_KERNEL_CONFIG := lavender_defconfig
 
