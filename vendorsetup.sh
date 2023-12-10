82ad4a3743cfaeec5e2c6890b2bcbf86f839dbe8
FebriCahyaa <febricahya12345@gmail.com>
2023-12-11 03:46:04 +0700

    lavender: Bringuo setup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,17 @@
 # Sepolicy
+rm -rf device/qcom/sepolicy
 git clone git@github.com:FebriCahyaa/android_device_qcom_sepolicy.git device/qcom/sepolicy
 git clone git@github.com:FebriCahyaa/android_device_sdm660_sepolicy.git device/sdm660/sepolicy
 git clone git@github.com:FebriCahyaa/android_device_sdm660_common.git device/sdm660/common
 
+# Hals
+rm -rf hardware/qcom-caf/sdm660/media;
+rm -rf hardware/qcom-caf/sdm660/display;
+rm -rf hardware/qcom-caf/sdm660/audio;
+git clone git@github.com:FebriCahyaa/android_hardware_qcom_media.git hardware/qcom-caf/sdm660/media
+git clone git@github.com:FebriCahyaa/android_hardware_qcom_display.git hardware/qcom-caf/sdm660/display
+git clone git@github.com:FebriCahyaa/android_hardware_qcom_audio.git hardware/qcom-caf/sdm660/audio
 
-
+# VT KT
+git clone https://github.com/ImSpiDy/kernel_xiaomi_sdm660.git --depth=1 kernel/xiaomi/lavender
+git clone https://github.com/RainbowUnicornXPe/proprietary_vendor_xiaomi_lavender.git vendor/xiaomi/lavender
